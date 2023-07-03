4f56bfba258db37ddd4d8ef0e4f1bf5cac0a5c18
ImJcm <wjsckdals0909@naver.com>
2023-07-03 20:03:41 +0900

    2023/07/03 20:03

--- Java/Queue&Graph/BOJ11724.java
+++ Java/Queue&Graph/BOJ11724.java
@@ -80,7 +80,7 @@ public class BOJ11724 {
         }
         System.out.println(connected_component);
 
-        /*connected_component = 0;
+        connected_component = 0;
         Arrays.fill(visited,false);
         //bfs
         for(int i=1;i<=N;i++) {
@@ -89,9 +89,16 @@ public class BOJ11724 {
             bfs(i);
             connected_component++;
         }
-        System.out.println(connected_component);*/
+        System.out.println(connected_component);
     }
 
+    /*
+        start 노드에서 시작하여 모든 노드와 연결상태와 방문여부를 확인하고, 방문하지 않았고, 연결된 상태의 노드가 있다면,
+        해당 노드를 방문 체크 후 위의 과정을 dfs 재귀호출을 통해 가능할 때까지 수행한다.
+        이때, dfs가 불가능하다면, 연결 상태 수를 증가시킨다.
+        => i(1)부터 시작하여 연결된 노드들을 모두 검사하고 dfs가 종료될 때는 더이상의 연결이 가능한 노드들이 없다는 의미이기 때문에
+            i+1 ~ N까지 dfs()를 호출하여 다른 연결상태의 노드들을 찾는 과정을 거쳐서 모든 노드가 방문될 때까지 연결상태 수를 +1 수행한다.
+     */
     static void dfs(int start) {
         for(int i=1;i<node[start].length;i++) {
             if(visited[i]) continue;
@@ -101,6 +108,11 @@ public class BOJ11724 {
         }
     }
 
+    /*
+        bfs형태로 start노드와 방문하지 않고 연결된 노드들을 queue에 담고 방문여부를 체크해준다. 이 과정을
+        queue가 비워질 때 까지 수행한다. 위 과정이 끝난다면 하나의 연결 상태를 만든 것이므로 연결상태 수를 +1
+        그리고, 방문하지 않는 노드가 아직 존재한다면 bfs(index)를 호출하여 다음 연결 상태를 검사하는 과정을 수행한다.
+     */
     static void bfs(int start) {
         queue = new LinkedList<>();
         queue.offer(start);
