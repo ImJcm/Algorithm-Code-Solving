c1ed996fd8c6d1a4ed6f57c4cdfc2aaafb215d19
ImJcm <wjsckdals0909@naver.com>
2024-10-31 19:17:34 +0900

    2024/10/31 19:17

--- Java/bruteforce/BOJ17779.java
+++ Java/bruteforce/BOJ17779.java
@@ -17,18 +17,18 @@ import java.util.ArrayList;
 
 선거구를 나누는 방법은 다음과 같다.
 
-기준점 (x, y)와 경계의 길이 d1, d2를 정한다. (d1, d2 ≥ 1, 1 ≤ x < x+d1+d2 ≤ N, 1 ≤ y-d1 < y < y+d2 ≤ N)
-다음 칸은 경계선이다.
+1. 기준점 (x, y)와 경계의 길이 d1, d2를 정한다. (d1, d2 ≥ 1, 1 ≤ x < x+d1+d2 ≤ N, 1 ≤ y-d1 < y < y+d2 ≤ N)
+2. 다음 칸은 경계선이다.
 (x, y), (x+1, y-1), ..., (x+d1, y-d1)
 (x, y), (x+1, y+1), ..., (x+d2, y+d2)
 (x+d1, y-d1), (x+d1+1, y-d1+1), ... (x+d1+d2, y-d1+d2)
 (x+d2, y+d2), (x+d2+1, y+d2-1), ..., (x+d2+d1, y+d2-d1)
-경계선과 경계선의 안에 포함되어있는 곳은 5번 선거구이다.
-5번 선거구에 포함되지 않은 구역 (r, c)의 선거구 번호는 다음 기준을 따른다.
-1번 선거구: 1 ≤ r < x+d1, 1 ≤ c ≤ y
-2번 선거구: 1 ≤ r ≤ x+d2, y < c ≤ N
-3번 선거구: x+d1 ≤ r ≤ N, 1 ≤ c < y-d1+d2
-4번 선거구: x+d2 < r ≤ N, y-d1+d2 ≤ c ≤ N
+3. 경계선과 경계선의 안에 포함되어있는 곳은 5번 선거구이다.
+4. 5번 선거구에 포함되지 않은 구역 (r, c)의 선거구 번호는 다음 기준을 따른다.
+    - 1번 선거구: 1 ≤ r < x+d1, 1 ≤ c ≤ y
+    - 2번 선거구: 1 ≤ r ≤ x+d2, y < c ≤ N
+    - 3번 선거구: x+d1 ≤ r ≤ N, 1 ≤ c < y-d1+d2
+    - 4번 선거구: x+d2 < r ≤ N, y-d1+d2 ≤ c ≤ N
 아래는 크기가 7×7인 재현시를 다섯 개의 선거구로 나눈 방법의 예시이다.
 
 
@@ -101,6 +101,24 @@ x = 2, y = 4, d1 = 2, d2 = 2	x = 2, y = 5, d1 = 3, d2 = 2	x = 4, y = 3, d1 = 1, 
 따라서, 처음부터 5의 경계를 구분하는 값으로 배열을 만들고, 나머지 구역은 조건에 맞게 구역을 나눌 수 있었다.
 (해당 좌표가 5인지 아닌지 여부를 검사하고 5인 값이 나오면, 그 이후 좌표는 다음 5의 좌표가 나올 때까지 5의 구역임을 확정지을 수 있고, 5가 나오지 않은 좌표는 x좌표, y좌표에 따라 각 구역을
 나타내면 된다.)
+
++ 1,2,3,4,5 구역의 넘버링을 하는 과정에서 예외가 발생하였다.
+경계선을 구분하여 구역을 나누지 않을 경우, 1-4, 2-3의 구역을 정하는 과정에서 각 조건을 만족하는 r,c가 동시에 존재하기 때문에 경계 측정이 잘못된 결과를 나타낸다.
+반례
+N = 6, x = 1, y = 2, d1 = 1, d2 = 4
+[1, 5, 2, 2, 2, 2]
+[5, 5, 5, 2, 2, 2]
+[3, 5, 5, 5, 2, 2]
+[3, 3, 5, 5, 5, 2]
+[3, 3, '2', 5, 5, 5]
+[3, 3, 3, 3, 5, 4]
+(5,3) 위치에 3의 넘버링이 되어야 하는데 2번 넘버링 조건과 3번 넘버링 조건이 동시에 만족하는 경우가 발생하여 2번 넘버링 수행 후 continue로 3번 넘버링이 불가능하다.
+
+따라서, 넘버링하는 과정에서 경계선 조건을 추가가 필요하다.
+
+이때, 경계선의 조건으로 5번 경계선의 기준이 r,c의 좌표값을 더하거나 뺀 값이 일정한 값을 유지하는 규칙을 발견하여 이를 이용하였다.
+각 좌표의 r,c를 더한 값이 / 방향으로 일정한 값을 유지하기 때문에 x,y의 좌표값을 기준으로 더한 값보다 작으면 1번 구역으로 측정하고, 4번 경계선으로 x + y + d2 값보다 큰 경우 4번 구역으로 측정한다.
+2,3번 구역의 경우 경계선이 r,c의 덧셈으로 일정한 값이 유지되지 않았고 뺄셈으로 일정한 값이 유지된다는 점을 이용하여 x - y 값보다 작은 경우 2번 구역으로 측정하고, (x + d1) - (y - d1) 값보다 큰 경우 3번 구역으로 측정하였다.
  */
 public class BOJ17779 {
     static class BOJ17779_divide_electoral_district_standard {
@@ -143,6 +161,8 @@ public class BOJ17779 {
 
         int[][] sections = divide_section_number(s);
 
+        System.out.println("break");
+
         for(int r=1;r<=N;r++) {
             for(int c=1;c<=N;c++) {
                 switch (sections[r][c]) {
@@ -180,6 +200,11 @@ public class BOJ17779 {
 
     static int[][] divide_section_number(BOJ17779_divide_electoral_district_standard s) {
         int[][] ns = new int[N+1][N+1];
+        /*int boundary_1 = s.x + s.y;
+        int boundary_2 = s.x - s.y;
+        int boundary_3 = (s.x + s.d1) - (s.y - s.d1);
+        int boundary_4 = s.x + s.y + s.d2;*/
+
 
         // 경계 표현
         // ↗, ↙
@@ -216,11 +241,29 @@ public class BOJ17779 {
                         ns[r][c] = 3;
                         continue;
                     }
-
                     if(r > s.x+s.d2 && c >= s.y-s.d1+s.d2) {
                         ns[r][c] = 4;
                         continue;
                     }
+
+                    /*if(r < s.x+s.d1 && c <= s.y && r + c < boundary_1) {
+                        ns[r][c] = 1;
+                        continue;
+                    }
+
+                    if(r <= s.x+s.d2 && c > s.y && r - c < boundary_2) {
+                        ns[r][c] = 2;
+                        continue;
+                    }
+
+                    if(r >= s.x+s.d1 && c < s.y-s.d1+s.d2 && r - c > boundary_3) {
+                        ns[r][c] = 3;
+                        continue;
+                    }
+                    if(r > s.x+s.d2 && c >= s.y-s.d1+s.d2 && r + c > boundary_4) {
+                        ns[r][c] = 4;
+                        continue;
+                    }*/
                 } else {
                     if(r == s.x || r == s.x+s.d1+s.d2) continue;
                     five_section = !five_section;
@@ -232,11 +275,11 @@ public class BOJ17779 {
     }
 
     static void divide_electoral_district_method() {
-        for(int x=1;x<N-1;x++) {
-            for(int y=2;y<N;y++) {
+        for(int x=1;x<=N;x++) {
+            for(int y=1;y<=N;y++) {
                 for(int d1=1;x+d1<=N;d1++) {
                     for(int d2=1;y+d2<=N;d2++) {
-                        if(x+d1+d2<=N && x+d2<=N && y-d1>=1) {
+                        if(x+d1+d2<=N && y-d1>=1) {
                             methods.add(new BOJ17779_divide_electoral_district_standard(x,y,d1,d2));
                         }
                     }
