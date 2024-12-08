1e1c3aa31e1f000c105eb7f44e86c4b75e565d04
ImJcm <wjsckdals0909@naver.com>
2024-12-08 23:36:26 +0900

    2024/12/08 23:36

---
+++ Java/DP/BOJ12026.java
@@ -0,0 +1,114 @@
+package BackJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/*
+BOJ 거리
+
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	512 MB	3818	2300	1916	62.067%
+문제
+BOJ 거리는 보도블록 N개가 일렬로 놓여진 형태의 도로이다. 도로의 보도블록은 1번부터 N번까지 번호가 매겨져 있다.
+
+스타트의 집은 1번에 있고, 링크의 집은 N번에 있다. 스타트는 링크를 만나기 위해서 점프해가려고 한다.
+
+BOJ거리의 각 보도블록에는 B, O, J 중에 하나가 쓰여 있다. 1번은 반드시 B이다.
+
+스타트는 점프를 통해서 다른 보도블록으로 이동할 수 있다. 이때, 항상 번호가 증가하는 방향으로 점프를 해야 한다. 만약, 스타트가 현재 있는 곳이 i번이라면, i+1번부터 N번까지로 점프를 할 수 있다. 한 번 k칸 만큼 점프를 하는데 필요한 에너지의 양은 k*k이다.
+
+스타트는 BOJ를 외치면서 링크를 만나러 가려고 한다. 따라서, 스타트는 B, O, J, B, O, J, B, O, J, ... 순서로 보도블록을 밟으면서 점프를 할 것이다.
+
+스타트가 링크를 만나는데 필요한 에너지 양의 최솟값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 1 ≤ N ≤ 1,000이 주어진다.
+
+둘째 줄에는 보도블록에 쓰여 있는 글자가 1번부터 순서대로 주어진다.
+
+출력
+스타트가 링크를 만나는데 필요한 에너지 양의 최솟값을 출력한다. 만약, 스타트가 링크를 만날 수 없는 경우에는 -1을 출력한다.
+
+예제 입력 1
+9
+BOJBOJBOJ
+예제 출력 1
+8
+예제 입력 2
+9
+BOJBOJBOJ
+예제 출력 2
+8
+예제 입력 3
+8
+BJJOOOBB
+예제 출력 3
+-1
+예제 입력 4
+13
+BJBBJOOOJJJJB
+예제 출력 4
+50
+예제 입력 5
+2
+BO
+예제 출력 5
+1
+예제 입력 6
+15
+BJBOJOJOOJOBOOO
+예제 출력 6
+52
+출처
+문제를 만든 사람: baekjoon
+데이터를 추가한 사람: djm03178
+문제의 오타를 찾은 사람: doju, jh05013
+알고리즘 분류
+다이나믹 프로그래밍
+ */
+public class BOJ12026 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N,ans;
+    static char[] blocks;
+
+
+    public static void main(String[] args) throws IOException {
+        init_setting();
+
+        solve();
+    }
+
+    private static void solve() {
+        //dfs(0,0,0);
+
+        //System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+    }
+
+    private static void dfs(int depth, int now_target, int total_energy) {
+        if(depth == N - 1) {
+            ans = Math.min(ans, total_energy);
+            return;
+        }
+
+        int next_target = (now_target + 1) % 3; // B = 0, O = 1, J = 2
+
+        for(int i = depth + 1; i < N; i++) {
+            int now = blocks[i] == 'B' ? 0 : blocks[i] == 'O' ? 1 : 2;
+
+            if(now != next_target) continue;
+
+            int e = (int) Math.pow(i - depth,2);
+            dfs(i,next_target,total_energy + e);
+        }
+    }
+
+    private static void init_setting() throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        blocks = br.readLine().toCharArray();
+
+        ans = Integer.MAX_VALUE;
+    }
+}
