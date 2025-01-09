3200bee8971e8eae729cfe3804185327c60cef9a
ImJcm <wjsckdals0909@naver.com>
2025-01-09 22:03:27 +0900

    2025/01/09 22:03

--- Java/DP/BOJ1970.java
+++ Java/DP/BOJ1970.java
@@ -41,9 +41,22 @@ ICPC > Regionals > Europe > Northwestern European Regional Contest > Benelux Alg
 알고리즘 분류
 다이나믹 프로그래밍
  */
+/*
+알고리즘 핵심
+DP
+처음 접근으로 시작지점과 끝지점 간의 최대 쌍의 개수를 구하는 것이기 때문에 2차원 DP를 생각하였고,
+상향식을 생각하여 0부터 시작하여 N-1까지 끝 지점을 정하는 반복문과 0부터 끝 지점을 구간에서 한 지점을 정하는 반복문으로 DP를 채울 수 있다고 생각하였지만 실패하였다.
+
+도저히 해결 방법이 떠오르지 않아 정답 코드 참고
+https://github.com/junhaa/JAVA/commit/773f7653d450fd873c65be7b10a2b56e0fc5132d - 하향식
+https://github.com/yunjaena/CSE_Study/commit/5ea121f8435ad3fca3ffef8f69fa106191b36a1d - 상향식
+
+상향식 방법을 2차원의 반복문으로 사용하는 것까지는 생각했으나 0부터 끝 지점까지의 반복문과 끝 지점을 결정하는 반복문 + 시작 지점과 끝 지점사이에서 최대 쌍 개수를 구하는
+반복문을 고려하지 못했다고 생각한다.
+ */
 public class BOJ1970 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int N;
+    static int N, ans;
     static int[] brands;
     static int[][] dp;
 
@@ -51,55 +64,68 @@ public class BOJ1970 {
         init_setting();
 
         solve();
+        solve2();
     }
 
-    private static void solve() {
-        /*for(int i = 0; i < N - 1; i++) {
-            for(int j = i + 1; j < N; j++) {
-                int e = 0;
-                if(brands[i] == brands[j]) e = 1;
-                int inner_i = i + 1;
-                int outer_i = j + 1 == N ? 0 : j + 1;
-                int inner_j = j - 1;
-                int outer_j = i - 1 == -1 ? N - 1 : i - 1;
+    /*
+        상향식 DP
+        i : 시작 지점과 끝 지점 사이의 구간 길이
+        s : 시작 지점
+        e : 끝 지점 (상대적 위치)
+        e_in_round : 원형 테이블에서의 인덱스 번호 (절대적 위치)
+     */
+    private static void solve2() {
+        for(int i = 0; i < N; i++) Arrays.fill(dp[i], 0);
 
-                dp[i][j] = Math.max(dp[i][j], dp[inner_i][inner_j] + dp[outer_i][outer_j] + e);
-            }
-        }*/
-        /*for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                int e = 0;
-                if((i != j) && (brands[i] == brands[j])) e = 1;
-                int inner_i = i + 1 == N ? 0 : i + 1;
-                int outer_i = i - 1 == -1 ? N - 1 : i - 1;
-                int inner_j = j - 1 == -1 ? N - 1 : j - 1;
-                int outer_j = j + 1 == N ? 0 : j + 1;
+        for(int i = 2; i <= N; i++) {
+            for(int s = 0; s < N; s++) {
+                int e = s + i - 1;
+                int e_in_round = e % N;
 
-                //dp[i][j] = Math.max(dp[i][j], dp[inner_i][inner_j] + dp[outer_i][outer_j] + e);
-                dp[i][j] = Math.max(dp[i][j],
-                        Math.max(dp[j][i], dp[inner_i][inner_j] + dp[outer_i][outer_j] + e));
+                // 시작 지점과 끝 지점의 콜라 브랜드가 같으면 (시작 지점 + 1, 끝 지점 - 1)의 dp + 1과 큰 값을 업데이트
+                if(brands[s] == brands[e_in_round]) {
+                    dp[s][e_in_round] = Math.max(dp[s][e_in_round], dp[(s + 1) % N][(e_in_round + N - 1) % N] + 1);
+                }
 
-                dp[j][i] = dp[i][j];
-            }
-        }*/
-
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                int e = 0;
-                if(i != j && brands[i] == brands[j]) e = 1;
-                int next_i = i;
-                int next_j = j;
-
-
-                int r = 0;
-                if(next_i < next_j) r = dp[next_i][next_j] + e;
-                else r = dp[next_j][next_i] + e;
-
-                dp[i][j] = Math.max(dp[i][j], r);
+                //시작 지점과 끝 지점 사이에서 한 점을 기준으로 (s,e) / (s, m) + (m + 1, e_in_round) 값 중 큰 값으로 업데이트
+                for(int m = s; m < e; m++) {
+                    dp[s][e_in_round] = Math.max(dp[s][e_in_round], dp[s][m % N] + dp[(m + 1) % N][e_in_round]);
+                }
             }
         }
 
-        System.out.println("break");
+        System.out.println(dp[0][N - 1]);
+    }
+
+    /*
+        하향식 DP
+     */
+    private static void solve() {
+        ans = dfs_dp(0,N - 1);
+
+        System.out.println(ans);
+    }
+
+    /*
+        s : 시작 지점
+        e : 끝 지점
+        끝 지점을 시작으로 시작 지점을 +1 씩 늘려가며 s와 e 내부에서 i값을 지정하여 (s,e)의 값을 s와 i의 콜라 브랜드가 같은 경우
+        (s,e) / (s + 1, i - 1) + (i + 1, e) + 1 중 큰 값으로 업데이트
+     */
+    private static int dfs_dp(int s, int e) {
+        if(s >= e) return 0;
+        if(dp[s][e] != -1) return dp[s][e];
+
+        int r = dfs_dp(s + 1, e);
+
+        for(int i = s + 1; i <= e; i++) {
+            if(brands[s] == brands[i]) {
+                r = Math.max(r, dfs_dp(s + 1, i - 1) + dfs_dp(i + 1,e) + 1);
+            }
+        }
+        dp[s][e] = r;
+
+        return dp[s][e];
     }
 
     private static void init_setting() throws IOException {
@@ -111,6 +137,6 @@ public class BOJ1970 {
 
         dp = new int[N][N];
 
-        //for(int i = 0; i < N; i++) Arrays.fill(dp[i], -1);
+        for(int i = 0; i < N; i++) Arrays.fill(dp[i], -1);
     }
 }
