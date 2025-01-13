2118de24d6b86a5b3491fd9f16e5e37fd4f0cd9c
ImJcm <wjsckdals0909@naver.com>
2025-01-13 23:44:53 +0900

    2025/01/13 23:44

---
+++ Java/DP/BOJ2163.java
@@ -0,0 +1,89 @@
+package BackJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/*
+초콜릿 자르기
+
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	128 MB	46604	32428	28801	71.161%
+문제
+정화는 N×M 크기의 초콜릿을 하나 가지고 있다. 초콜릿은 금이 가 있는 모양을 하고 있으며, 그 금에 의해 N×M개의 조각으로 나눠질 수 있다.
+
+초콜릿의 크기가 너무 크다고 생각한 그녀는 초콜릿을 친구들과 나눠 먹기로 했다. 이를 위해서 정화는 초콜릿을 계속 쪼개서 총 N×M개의 조각으로 쪼개려고 한다. 초콜릿을 쪼갤 때에는 초콜릿 조각을 하나 들고, 적당한 위치에서 초콜릿을 쪼갠다. 초콜릿을 쪼갤 때에는 금이 가 있는 위치에서만 쪼갤 수 있다. 이와 같이 초콜릿을 쪼개면 초콜릿은 두 개의 조각으로 나눠지게 된다. 이제 다시 이 중에서 초콜릿 조각을 하나 들고, 쪼개는 과정을 반복하면 된다.
+
+초콜릿을 쪼개다보면 초콜릿이 녹을 수 있기 때문에, 정화는 가급적이면 초콜릿을 쪼개는 횟수를 최소로 하려 한다. 초콜릿의 크기가 주어졌을 때, 이를 1×1 크기의 초콜릿으로 쪼개기 위한 최소 쪼개기 횟수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 두 정수 N, M(1 ≤ N, M ≤ 300)이 주어진다.
+
+출력
+첫째 줄에 답을 출력한다.
+
+예제 입력 1
+2 2
+예제 출력 1
+3
+예제 입력 2
+1 1
+예제 출력 2
+0
+출처
+문제의 오타를 찾은 사람: gusqja8753
+알고리즘 분류
+수학
+사칙연산
+ */
+public class BOJ2163 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N,M,ans;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        init_setting();
+
+        solve();
+    }
+
+    private static void solve() {
+        ans = dfs_dp(N,M);
+
+        System.out.println(ans);
+    }
+
+    private static int dfs_dp(int n, int m) {
+        if(n == 1 && m == 1) return 0;
+
+        int nn = Math.max(n,m);
+        int nm = Math.min(n,m);
+
+        if(dp[nn][nm] != Integer.MAX_VALUE) return dp[nn][nm];
+
+        int r = dp[nn][nm];
+
+        for(int i = 1; i < nn; i++) {
+           r = Math.min(r, dfs_dp(i, nm) + dfs_dp(nn - i, nm) + 1);
+        }
+
+        dp[nn][nm] = r;
+
+        return dp[nn][nm];
+    }
+
+    private static void init_setting() throws IOException {
+        String[] input = br.readLine().split(" ");
+
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        ans = 0;
+
+        int max_length = Math.max(N,M);
+        dp = new int[max_length + 1][max_length + 1];
+
+        for(int n = 0; n < max_length + 1; n++) Arrays.fill(dp[n], Integer.MAX_VALUE);
+    }
+}
