91626edd572d78413de4bd943abf76fe9b514cb5
ImJcm <wjsckdals0909@naver.com>
2025-03-21 00:18:48 +0900

    2025/03/21 00:18

--- Java/DP/BOJ1003.java
+++ Java/DP/BOJ1003.java
@@ -68,12 +68,46 @@ fibonacci(3)은 fibonacci(2)와 fibonacci(1)의 결과를 얻고, 2를 리턴한
 public class BOJ1003 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int T;
+    static int[] dp_zero,dp_one;
 
     public static void main(String[] args) throws IOException{
         init_setting();
+
+        solve();
+    }
+
+    private static int fibonacci(int n) {
+        if(n == 0) return 0;
+        else if(n == 1) return 1;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    private static void solve() throws IOException{
+        for(int i = 3; i <= 40; i++) {
+            dp_zero[i] = dp_zero[i - 1] + dp_zero[i - 2];
+            dp_one[i] = dp_one[i - 1] + dp_one[i - 2];
+        }
+
+        for(int i = 0; i < T; i++) {
+            int t = Integer.parseInt(br.readLine());
+
+            System.out.println(dp_zero[t] + " " + dp_one[t]);
+        }
     }
 
     private static void init_setting() throws IOException {
+        T = Integer.parseInt(br.readLine());
 
+        dp_zero = new int[41];
+        dp_one = new int[41];
+
+        dp_zero[0] = 1;
+        dp_one[0] = 0;
+
+        dp_zero[1] = 0;
+        dp_one[1] = 1;
+
+        dp_zero[2] = 1;
+        dp_one[2] = 1;
     }
 }
