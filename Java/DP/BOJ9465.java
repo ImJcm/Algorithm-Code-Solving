aed7a424fd28c33bc2f931970b69a33c6b614004
ImJcm <wjsckdals0909@naver.com>
2025-05-20 03:34:05 +0900

    2025/05/20 03:24

--- Java/DP/BOJ9465.java
+++ Java/DP/BOJ9465.java
@@ -1,6 +1,9 @@
 package BackJoon;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /*
 스티커 다국어
@@ -43,17 +46,82 @@ ICPC > Regionals > Asia Pacific > Korea > Asia Regional - Daejeon 2013 K번
 알고리즘 분류
 다이나믹 프로그래밍
  */
-public class BOJ9465 {
-    public static void main(String[] args) throws IOException {
-        init_setting();
+/*
+알고리즘 핵심
+DP
+1. 하나의 스티커를 고를 경우 상,하,좌,우의 스티커는 고를 수 없기 때문에 현재 스티커를 기준으로 최대 스코어를 가지는 경우는
+n - 2에서 최대 스코어를 가지는 누적값과 n - 1에서 고를 수 있는 스티커에서 누적된 점수의 최대값을 더한 경우이다.
+이를 정규식으로 나타내면 다음과 같다.
+dp(r,n) = score(r,n) + max(dp(!r,n-1), dp(r,n-2), dp(!r,n-2)) #(r = 0 or 1)
+2. ans = dp(r,N-1), dp(!r,N-1) 중 최대값을 저장하고 출력한다.
 
-        solve();
+#위 정규식을 바탕으로 상향식, 하향식 dp를 모두 구현하였다.
+ */
+public class BOJ9465 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int T,N,ans;
+    static int[][] sticker_point,dp;
+
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+
+        while(T-- > 0) {
+            init_setting();
+
+            ans = Math.max(solve_A(0,N - 1), solve_A(1,N - 1));
+            //solve();
+
+            System.out.println(ans);
+        }
     }
 
     private static void solve() {
+        int n1,n2,d1,d2,d3,d4;
 
+        for(int i = 0; i < N; i++) {
+            n1 = i - 2;
+            n2 = i - 1;
+
+            d1 = i - 2 < 0 ? 0 : dp[0][n1];
+            d2 = i - 2 < 0 ? 0 : dp[1][n1];
+            d3 = i - 1 < 0 ? 0 : dp[0][n2];
+            d4 = i - 1 < 0 ? 0 : dp[1][n2];
+
+            dp[0][i] = sticker_point[0][i] + Math.max(d1,
+                    Math.max(d2,d4));
+
+            dp[1][i] = sticker_point[1][i] + Math.max(d2,
+                    Math.max(d1,d3));
+        }
+
+        ans = Math.max(dp[0][N - 1], dp[1][N - 1]);
+    }
+
+    private static int solve_A(int r, int n) {
+        if(n < 0) {
+            return 0;
+        }
+
+        if(dp[r][n] != 0) return dp[r][n];
+
+        dp[r][n] = sticker_point[r][n] + Math.max(solve_A((r + 1) % 2, n - 1),
+                Math.max(solve_A(r, n - 2), solve_A((r + 1) % 2, n - 2)));
+
+        return dp[r][n];
     }
 
     private static void init_setting() throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        dp = new int[2][N];
+        sticker_point = new int[2][];
+
+        sticker_point[0] = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        sticker_point[1] = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
