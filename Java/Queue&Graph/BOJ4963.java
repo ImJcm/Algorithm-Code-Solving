2202054ab71b77a792315d8140cfa13918c21a1d
ImJcm <wjsckdals0909@naver.com>
2025-06-02 20:10:10 +0900

    init

---
+++ Java/Queue&Graph/BOJ4963.java
@@ -0,0 +1,93 @@
+package BackJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+섬의 개수 다국어
+
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	80324	41256	29622	50.242%
+문제
+정사각형으로 이루어져 있는 섬과 바다 지도가 주어진다. 섬의 개수를 세는 프로그램을 작성하시오.
+
+
+
+한 정사각형과 가로, 세로 또는 대각선으로 연결되어 있는 사각형은 걸어갈 수 있는 사각형이다.
+
+두 정사각형이 같은 섬에 있으려면, 한 정사각형에서 다른 정사각형으로 걸어서 갈 수 있는 경로가 있어야 한다. 지도는 바다로 둘러싸여 있으며, 지도 밖으로 나갈 수 없다.
+
+입력
+입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 테스트 케이스의 첫째 줄에는 지도의 너비 w와 높이 h가 주어진다. w와 h는 50보다 작거나 같은 양의 정수이다.
+
+둘째 줄부터 h개 줄에는 지도가 주어진다. 1은 땅, 0은 바다이다.
+
+입력의 마지막 줄에는 0이 두 개 주어진다.
+
+출력
+각 테스트 케이스에 대해서, 섬의 개수를 출력한다.
+
+예제 입력 1
+1 1
+0
+2 2
+0 1
+1 0
+3 2
+1 1 1
+1 1 1
+5 4
+1 0 1 0 0
+1 0 0 0 0
+1 0 1 0 1
+1 0 0 1 0
+5 4
+1 1 1 0 1
+1 0 1 0 1
+1 0 1 0 1
+1 0 1 1 1
+5 5
+1 0 1 0 1
+0 0 0 0 0
+1 0 1 0 1
+0 0 0 0 0
+1 0 1 0 1
+0 0
+예제 출력 1
+0
+1
+1
+3
+1
+9
+출처
+ICPC > Regionals > Asia Pacific > Japan > Japan Domestic Contest > 2009 Japan Domestic Contest B번
+
+문제를 번역한 사람: baekjoon
+문제의 오타를 찾은 사람: j4bez
+알고리즘 분류
+그래프 이론
+그래프 탐색
+너비 우선 탐색
+깊이 우선 탐색
+격자 그래프
+플러드 필
+ */
+public class BOJ4963 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        init_setting();
+
+        solve();
+    }
+
+    private static void solve() {
+
+    }
+
+    private static void init_setting() throws IOException {
+
+    }
+}
