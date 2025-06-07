695815eac81adf35184ea1b01b44aefc0ae8ca97
ImJcm <wjsckdals0909@naver.com>
2025-06-07 10:14:54 +0900

    2025/06/07 10:14

--- Java/DataStructure/BOJ1991.java
+++ Java/DataStructure/BOJ1991.java
@@ -47,8 +47,30 @@ DBEGFCA
 집합과 맵
 재귀
  */
+/*
+알고리즘 핵심
+자료구조 (tree - preorder, inorder, postorder)
+1. N(A,B,...)개의 root, left_child, right_child를 갖는 구조체를 만들고, 입력으로 주어지는 연결 상태를 업데이트한다.
+2. 전위 순환(preorder)의 경우, 메인 노드의 root가 먼저 탐색되는 구조이므로 main.root를 저장하고, left, right 순으로 재귀 탐색한다.
+중위 순환(inorder)의 경우, 메인 노드의 root가 두번째로 탐색되는 구조이므로 left 탐색을 마친 후, main.root를 저장하고, right 순서로 재귀 탐색한다.
+후위 순환(postorder)의 경우, 메인 노드의 root가 세번째로 탐색되는 구조이므로 left 탐색하고, right 탐색을 마친 후, main.root를 저장하고, 재귀 탐색한다.
+ */
 public class BOJ1991 {
+    static class BOJ1991_node {
+        char root;
+        BOJ1991_node left_child, right_child;
+
+        BOJ1991_node(char root, BOJ1991_node lc, BOJ1991_node rc) {
+            this.root = root;
+            this.left_child = lc;
+            this.right_child = rc;
+        }
+    }
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static String res;
+    static BOJ1991_node[] trees;
+    static StringBuilder ans;
 
     public static void main(String[] args) throws IOException {
         init_setting();
@@ -57,10 +79,69 @@ public class BOJ1991 {
     }
 
     private static void solve() {
+        BOJ1991_node main_root = trees[0];
 
+        res = new String();
+
+        preorder_traversal(main_root);
+        ans.append(res).append("\n");
+
+        res = "";
+        inorder_traversal(main_root);
+        ans.append(res).append("\n");
+
+        res = "";
+        postorder_traversal(main_root);
+        ans.append(res).append("\n");
+
+        System.out.println(ans.toString());
+    }
+
+    private static void preorder_traversal(BOJ1991_node node) {
+        res += node.root;
+        if(node.left_child != null) preorder_traversal(node.left_child);
+        if(node.right_child != null) preorder_traversal(node.right_child);
+    }
+
+    private static void inorder_traversal(BOJ1991_node node) {
+        if(node.left_child != null) inorder_traversal(node.left_child);
+        res += node.root;
+        if(node.right_child != null) inorder_traversal(node.right_child);
+    }
+
+    private static void postorder_traversal(BOJ1991_node node) {
+        if(node.left_child != null) postorder_traversal(node.left_child);
+        if(node.right_child != null) postorder_traversal(node.right_child);
+        res += node.root;
     }
 
     private static void init_setting() throws IOException {
+        N = Integer.parseInt(br.readLine());
 
+        trees = new BOJ1991_node[N];
+
+        char ch = 'A';
+
+        for(int n = 0; n < N; n++) {
+            trees[n] = new BOJ1991_node((char)(ch + n),null,null);
+        }
+
+        for(int i = 0; i < N; i++) {
+            String[] connect_info = br.readLine().split(" ");
+
+            int idx = connect_info[0].charAt(0) - 'A';
+
+            if(!connect_info[1].equals(".")) {
+                int l_idx = connect_info[1].charAt(0) - 'A';
+                trees[idx].left_child = trees[l_idx];
+            }
+
+            if(!connect_info[2].equals(".")) {
+                int r_idx = connect_info[2].charAt(0) - 'A';
+                trees[idx].right_child = trees[r_idx];
+            }
+        }
+
+        ans = new StringBuilder();
     }
 }
