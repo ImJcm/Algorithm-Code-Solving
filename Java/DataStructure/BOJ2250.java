fc2637f95c5b5687754e612014709f9f4c51f488
ImJcm <wjsckdals0909@naver.com>
2025-06-11 14:27:34 +0900

    2025/06/11 14:27

--- Java/DataStructure/BOJ2250.java
+++ Java/DataStructure/BOJ2250.java
@@ -3,6 +3,7 @@ package BackJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /*
 트리의 높이와 너비
@@ -69,14 +70,31 @@ Olympiad > 한국정보올림피아드 > KOI 2003 > 고등부 1번
 트리
 깊이 우선 탐색
  */
+/*
+알고리즘 핵심
+이진트리 + dfs
+1. 부모 노드, 좌측 자식 노드, 우측 자식 노드, 노드 식별 번호, 좌측 자식 노드의 총 개수, 우측 자식 노드의 총 개수 정보를 갖는 구조체를 사용한다.
+2. 입력으로 주어진 부모,자식 노드 관계를 설정한다.
+3. 입력으로 주어진 노드에서 1번 노드는 root 노드가 아니므로, 부모 노드가 없는 노드를 root 노드로 설정한다. - find_root_node()
+4. root 노드를 시작으로 각 노드를 기준으로 좌측 자식 노드, 우측 자식 노드의 개수를 dfs 탐색으로 찾고 node.left_child_cnt, node.right_child_cnt값을 업데이트한다. - dfs_left_right_child_node_cnt_and_level_node(root)
+5. 구성된 이진 트리에서 최대 레벨 값을 dfs를 통해 찾고, 해당 값으로 레벨 별 최대, 최소 열을 저장하는 배열을 만든다. - find_max_level_and_level_max_min_arr_setting()
+6. root 노드를 시작으로 자식 노드까지 dfs로 탐색하면서 각 노드들의 col 위치를 설정한다. - dfs_column_node(root,true);
+이때, 설정하는 방법은 부모노드의 col 위치를 기준으로 왼쪽 자식 노드이면, parent's col - 현재 노드의 우측 자식 노드의 개수 - 1로 설정할 수 있다.
+오른쪽 자식 노드이면, parent's col + 현재 노드의 좌측 자식 노드의 개수 + 1로 설정할 수 있다.
+7. root 노드를 시작으로 dfs로 자식 노드를 탐색하면서 level 별 노드의 현재 col 값을 최소, 최대 값을 업데이트한다. - dfs_same_level_node_width(root);
+8. 레벨 별 최소, 최대 열을 저장한 배열에서 두 값의 차이가 최대인 레벨과 너비의 차이를 ans, level_ans에 업데이트하고 출력한다. - cal_level_width()
+
+이 문제에서 중요한 부분은 문제의 답을 찾기 위해 각 과정에서 dfs가 사용된다는 점과 입력으로 주어진 예시에서 1이 항상 root 노드가 아닌 점을 알아야 한다.
+ */
 public class BOJ2250 {
     static class BOJ2250_node {
-        int root;
-        BOJ2250_node left_child, right_child;
+        int idx;
+        BOJ2250_node parent, left_child, right_child;
         int left_child_cnt, right_child_cnt;
 
-        BOJ2250_node(int root,BOJ2250_node lc, BOJ2250_node rc, int lcc, int rcc) {
-            this.root = root;
+        BOJ2250_node(int idx,BOJ2250_node p, BOJ2250_node lc, BOJ2250_node rc, int lcc, int rcc) {
+            this.idx = idx;
+            this.parent = p;
             this.left_child = lc;
             this.right_child = rc;
             this.left_child_cnt = lcc;
@@ -84,8 +102,10 @@ public class BOJ2250 {
         }
     }
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int N;
+    static int N,ans,ans_level,max_level;
     static BOJ2250_node[] nodes;
+    static int[] level_nodes,col_nodes;
+    static int[][] level_max_min_col_node;
 
     public static void main(String[] args) throws IOException {
         init_setting();
@@ -94,18 +114,77 @@ public class BOJ2250 {
     }
 
     private static void solve() {
-        dfs_left_right_child_node_cnt(nodes[1]);
+        BOJ2250_node root = find_root_node();
 
-        //make_arr
+        dfs_left_right_child_node_cnt_and_level_node(root);
 
-        System.out.println("breaking");
+        find_max_level_and_level_max_min_arr_setting();
+
+        dfs_column_node(root,true);
+
+        dfs_same_level_node_width(root);
+
+        cal_level_width();
+
+        System.out.println(ans_level + " " + ans);
     }
 
-    private static void dfs_left_right_child_node_cnt(BOJ2250_node node) {
+    private static BOJ2250_node find_root_node() {
+        BOJ2250_node root = null;
+
+        for(int i = 1; i <= N; i++) {
+            if(nodes[i].parent == null) root = nodes[i];
+        }
+
+        return root;
+    }
+
+    private static void cal_level_width() {
+        ans = 0;
+        for(int i = 1; i <= max_level; i++) {
+            if(ans < level_max_min_col_node[i][0] - level_max_min_col_node[i][1] + 1) {
+                ans = level_max_min_col_node[i][0] - level_max_min_col_node[i][1] + 1;
+                ans_level = i;
+            }
+        }
+    }
+
+    private static void dfs_same_level_node_width(BOJ2250_node node) {
         if(node == null) return;
 
-        dfs_left_right_child_node_cnt(node.left_child);
-        dfs_left_right_child_node_cnt(node.right_child);
+        int level = level_nodes[node.idx];
+        int col = col_nodes[node.idx];
+
+        level_max_min_col_node[level][0] = Math.max(level_max_min_col_node[level][0], col);
+        level_max_min_col_node[level][1] = Math.min(level_max_min_col_node[level][1], col);
+
+        dfs_same_level_node_width(node.left_child);
+        dfs_same_level_node_width(node.right_child);
+    }
+
+    private static void dfs_column_node(BOJ2250_node node, boolean isRight_subtree) {
+        if(node == null) return;
+
+        int col = node.parent == null ? 0 : col_nodes[node.parent.idx];
+
+        if(isRight_subtree) {
+            col_nodes[node.idx] = col + node.left_child_cnt + 1;
+        } else {
+            col_nodes[node.idx] = col - node.right_child_cnt - 1;
+        }
+
+        dfs_column_node(node.left_child, false);
+        dfs_column_node(node.right_child, true);
+    }
+
+    private static void dfs_left_right_child_node_cnt_and_level_node(BOJ2250_node node) {
+        if(node == null) return;
+
+        int parent_idx = node.parent == null ? 0 : node.parent.idx;
+        level_nodes[node.idx] = level_nodes[parent_idx] + 1;
+
+        dfs_left_right_child_node_cnt_and_level_node(node.left_child);
+        dfs_left_right_child_node_cnt_and_level_node(node.right_child);
 
         int lc = node.left_child == null ? 0 : node.left_child.left_child_cnt + node.left_child.right_child_cnt + 1;
         int rc = node.right_child == null ? 0 : node.right_child.left_child_cnt + node.right_child.right_child_cnt + 1;;
@@ -114,13 +193,25 @@ public class BOJ2250 {
         node.right_child_cnt = rc;
     }
 
+    private static void find_max_level_and_level_max_min_arr_setting() {
+        max_level = Arrays.stream(level_nodes).max().getAsInt();
+
+        level_max_min_col_node = new int[max_level + 1][2];
+
+        for(int[] n : level_max_min_col_node) {
+            n[1] = Integer.MAX_VALUE;
+        }
+    }
+
     private static void init_setting() throws IOException {
         N = Integer.parseInt(br.readLine());
 
         nodes = new BOJ2250_node[N + 1];
+        level_nodes = new int[N + 1];
+        col_nodes = new int[N + 1];
 
         for(int i = 1; i <= N; i++) {
-            nodes[i] = new BOJ2250_node(i,null,null,0,0);
+            nodes[i] = new BOJ2250_node(i,null,null,null,0,0);
         }
 
         for(int i = 1; i <= N; i++) {
@@ -130,8 +221,14 @@ public class BOJ2250 {
             int left_node = Integer.parseInt(connect_info[1]);
             int right_node = Integer.parseInt(connect_info[2]);
 
-            if(left_node != -1) nodes[root_node].left_child = nodes[left_node];
-            if(right_node != -1) nodes[root_node].right_child = nodes[right_node];
+            if(left_node != -1) {
+                nodes[root_node].left_child = nodes[left_node];
+                nodes[left_node].parent = nodes[root_node];
+            }
+            if(right_node != -1) {
+                nodes[root_node].right_child = nodes[right_node];
+                nodes[right_node].parent = nodes[root_node];
+            }
         }
     }
 }
