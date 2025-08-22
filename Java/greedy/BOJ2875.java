7ffbdc70cd03b06fdea762c375f881e33b30ba64
ImJcm <wjsckdals0909@naver.com>
2025-08-22 15:55:20 +0900

    2025/08/22 15:55

--- Java/greedy/BOJ2875.java
+++ Java/greedy/BOJ2875.java
@@ -46,8 +46,18 @@ Contest > Croatian Open Competition in Informatics > COCI 2010/2011 > Contest #1
 구현
 사칙연산
  */
+/*
+알고리즘 핵심
+그리디 알고리즘 + 수학
+1. 여학생 2명과 남학생 1명이 팀을 이루므로 n/2, m 중 최소값이 이룰 수 있는 팀의 수를 구한다.
+2. 팀을 구성한 후, 남은 여학생과 남학생의 수를 갱신한 후, 남은 학생의 수를 구한다.
+3. 남은 학생의 수로 인턴쉽 프로그램에 참여한 학생을 뽑기 위해 남은 학생의 수보다 인턴쉽에 참여할 학생이 적은 경우
+현재 팀을 줄이고 남은 학생의 수를 3명씩 증가시켜 K보다 많아질 때까지 팀을 줄인다.
+4. 위 상황을 만족할 때 팀의 수를 출력한다.
+ */
 public class BOJ2875 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N,M,K,ans;
 
     public static void main(String[] args) throws IOException {
         init_setting();
@@ -56,10 +66,30 @@ public class BOJ2875 {
     }
 
     private static void solve() {
+        int T = Math.min(N / 2, M);
 
+        N -= T * 2;
+        M -= T;
+
+        int S = N + M;
+
+        while(T > 0 && K > S) {
+            T--;
+            S += 3;
+        }
+
+        ans = K > S ? -1 : T;
+
+        System.out.println(ans);
     }
 
     private static void init_setting() throws IOException {
+        String[] input = br.readLine().split(" ");
 
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        K = Integer.parseInt(input[2]);
+
+        ans = 0;
     }
 }
