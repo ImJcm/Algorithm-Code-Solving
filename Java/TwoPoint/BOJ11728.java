9cc51ab16a44f1bbcf0bde5ea9dcc7757a11b472
ImJcm <wjsckdals0909@naver.com>
2025-09-17 18:46:24 +0900

    2025/09/17 18:46

--- Java/TwoPoint/BOJ11728.java
+++ Java/TwoPoint/BOJ11728.java
@@ -3,6 +3,8 @@ package BackJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /*
 배열 합치기
@@ -44,8 +46,17 @@ import java.io.InputStreamReader;
 정렬
 두 포인터
  */
+/*
+알고리즘 핵심
+투 포인트
+1. A,B의 정렬된 배열에서 각각의 A_idx, B_idx를 가지고 A[A_idx], B[B_idx] 값을 비교하여 작은 값을 우선하여 ans에 저장한다.
+2. A or B에서 idx가 길이를 넘긴 경우, 나머지 배열에서 값을 ans에 추가한다.
+ */
 public class BOJ11728 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N,M,A_idx,B_idx;
+    static int[] A,B;
+    static StringBuilder ans;
 
     public static void main(String[] args) throws IOException {
         init_setting();
@@ -54,10 +65,34 @@ public class BOJ11728 {
     }
 
     private static void solve() {
+        while(A_idx < A.length && B_idx < B.length) {
+            if(A[A_idx] < B[B_idx]) ans.append(A[A_idx++]).append(" ");
+            else ans.append(B[B_idx++]).append(" ");
+        }
 
+        if(A_idx == A.length) for(int i = B_idx; i < B.length; i++) ans.append(B[i]).append(" ");
+        if(B_idx == B.length) for(int i = A_idx; i < A.length; i++) ans.append(A[i]).append(" ");
+
+        System.out.println(ans.toString());
     }
 
     private static void init_setting() throws IOException {
+        String[] input = br.readLine().split(" ");
 
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        A = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        B = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        A_idx = 0;
+        B_idx = 0;
+
+        ans = new StringBuilder();
     }
 }
