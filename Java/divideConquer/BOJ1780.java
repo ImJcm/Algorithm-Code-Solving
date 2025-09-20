e90c49403805e398c8be4ae3fb2169f2ef9be655
ImJcm <wjsckdals0909@naver.com>
2025-09-20 14:20:11 +0900

    2025/09/20 14:20

--- Java/divideConquer/BOJ1780.java
+++ Java/divideConquer/BOJ1780.java
@@ -3,6 +3,7 @@ package BackJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /*
 종이의 개수
@@ -45,20 +46,74 @@ N×N크기의 행렬로 표현되는 종이가 있다. 종이의 각 칸에는 -
 분할 정복
 재귀
  */
+/*
+알고리즘 핵심
+분할 정복 + 재귀
+1. N과 시작 좌표 r,c을 시작으로 (r ~ r + N, c ~ c + N)의 수가 모두 같은지 확인한다.
+2. 같다면 (r,c)의 수인 종이를 +1한다
+3. 같지 않다면, N = N / 3으로 나눈 후, (r,c)를 각각 9개의 ((r + N/3 * [1,2,3]),(c + N/3 * [1,2,3])) 종이를 만들고
+1번 과정을 반복한다.
+4. 2번 과정에서 조건을 만족한다면 해당 종이는 1번 과정을 거치지 않는다. 이후, N = 1이 될때까지 반복하여 ans를 업데이트한다.
+ */
 public class BOJ1780 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    static int N;
+    static int[] ans;
+    static int[][] papers;
     public static void main(String[] args) throws IOException {
         init_setting();
 
         solve();
     }
 
-    private static void solve() {
+    private static boolean is_same_value_paper(int n, int r, int c) {
+        int num = papers[r][c];
 
+        for(int i = r; i < r + n; i++) {
+            for(int j = c; j < c + n; j++) {
+                if(num != papers[i][j]) return false;
+            }
+        }
+
+        ans[num + 1] += 1;
+        return true;
+    }
+
+    private static void divide_conquer(int n, int r, int c) {
+        if(is_same_value_paper(n,r,c)) return;
+        else {
+            int nn = n / 3;
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    divide_conquer(nn, r + (nn * i), c + (nn * j));
+                }
+            }
+        }
+    }
+
+    private static void solve() {
+        divide_conquer(N,0,0);
+
+        print();
+    }
+
+    private static void print() {
+        for(int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
     }
 
     private static void init_setting() throws IOException {
+        N = Integer.parseInt(br.readLine());
 
+        papers = new int[N][];
+
+        for(int i = 0; i < N; i++) {
+            papers[i] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        ans = new int[3];
     }
 }
