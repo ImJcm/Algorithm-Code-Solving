ffc25ff3c676382d70842dd13d56facb9cf0f43d
ImJcm <wjsckdals0909@naver.com>
2025-12-22 07:59:29 +0900

    2025/12/22 07:58

--- Java/binarySearch/BOJ1981.java
+++ Java/binarySearch/BOJ1981.java
@@ -44,7 +44,12 @@ Olympiad > USA Computing Olympiad > 2002-2003 Season > USACO US Open 2003 Contes
 너비 우선 탐색
  */
 /*
-
+알고리즘 핵심
+이분탐색 + BFS
+1. 입력으로 주어지는 값중 최대,최소의 뺀값으로 만들 수 있는 결과값을 이분탐색을 진행하고, 최소값을 지정(0~MAX_VALUE)하여 BFS를 수행한다.
+2. BFS의 수행결과가 true인 경우, 최대-최소 + 최소값을 만족하는 경로가 존재하므로 최대-최소값을 줄이기 위해 r = m - 1로 설정한다.
+BFS의 수행결과가 false인 경우, 만족하는 경로가 없으므로 l = m + 1로 설정한다.
+3. 2번의 과정을 수행하는데 최소값을 지정하기 때문에 0~MAX_VALUE 횟수의 반복문으로 수행해야 한다.
  */
 public class BOJ1981 {
     public static void main(String[] args) throws IOException {
@@ -52,7 +57,338 @@ public class BOJ1981 {
         task.solve();
     }
 
+    /*
+        정답 코드 : 입력으로 주어지는 값중 최소값과 최대값을 기준으로 두 값의 합 / 2를 이분 탐색하여 값을 정하고, 경로를 탐색하는 과정에서 최소값을 지정하여
+        max-min이 이분탐색한 결과와 같은지 여부를 통해 l,r을 조정한다.
+        bfs 과정에서 현시점에서 다음으로 나아가는 위치의 최소값과 최대값의 차이를 비교하는 것외에 현재 경로를 탐색하는 일련의 과정에서 최소값을 정하는 가정이 필요하다.
+        결국 최소값을 정하여 bfs를 수행하기 때문에 max의 값과 정해진 min값과의 차이를 이분탐색의 결과값과 비교하여 (N,N)에 도달할 수 있는지를 검사한다.
+        최소값을 정하는 이유는 입력으로 주어지는 값중 최대-최소인 값으로 이분탐색한 결과값만으로 최소결과를 도출할 수 없다.
+        => (한번의 bfs로 해당하는 경로의 최대-최소인 값이 최소임을 만족하지 못하기 때문에 이미 탐색한 곳을 중복으로 탐색하여 경로를 만들어야하지만 중복 탐색 경로를
+        정하는 기준이 애매하다. + 최소값을 결정함으로써 가능한 경로가 나뉘어지고 최대-최소값이 달라질 수 있다.)
+        힌트 참고 : https://www.acmicpc.net/board/view/5577
+     */
     public static class Solve {
+        public class Pos {
+            int x,y,n,min_n,max_n;
+
+            Pos(int x, int y, int n, int min, int max) {
+                this.x = x;
+                this.y = y;
+                this.n = n;
+                this.min_n = min;
+                this.max_n = max;
+            }
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N,ans,l,r,max_r;
+        int[][] direction = {{0,-1},{-1,0},{0,1},{1,0}};
+        Pos[][] arr;
+
+        private void solve() throws IOException {
+            init_setting();
+
+            binary_search();
+
+            System.out.println(ans);
+        }
+
+        private void binary_search() {
+            if(l > r) return;
+
+            int m = (l + r) / 2;
+            boolean flag = false;
+
+            for(int s = 0; s <= max_r; s++) {
+                if(bfs(arr[0][0], m, s)) {
+                    flag = true;
+                    ans = m;
+                }
+            }
+
+            if(flag) r = m - 1;
+            else l = m + 1;
+
+            binary_search();
+        }
+
+        private boolean bfs(Pos s, int m, int min_n) {
+            PriorityQueue<Pos> q = new PriorityQueue<>(new Comparator<Pos>() {
+                @Override
+                public int compare(Pos o1, Pos o2) {
+                    return o1.min_n - o2.min_n;
+                }
+            });
+            boolean[][] visited = new boolean[N][N];
+
+            q.add(s);
+            visited[s.x][s.y] = true;
+
+            while(!q.isEmpty()) {
+                Pos np = q.poll();
+
+                if(np.x == N - 1 && np.y == N - 1) {
+                    return true;
+                }
+
+                for(int[] d : direction) {
+                    int nx = np.x + d[0];
+                    int ny = np.y + d[1];
+
+                    if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+                    int nmin_n = Math.min(np.min_n, arr[nx][ny].n);
+                    int nmax_n = Math.max(np.max_n, arr[nx][ny].n);
+                    int diff = nmax_n - min_n;
+
+                    if(nmin_n < min_n || diff > m || visited[nx][ny]) continue;
+
+                    visited[nx][ny] = true;
+                    q.add(new Pos(nx,ny,arr[nx][ny].n,nmin_n,nmax_n));
+                }
+            }
+            return false;
+        }
+
+        private void init_setting() throws IOException {
+            N = Integer.parseInt(br.readLine());
+
+            arr = new Pos[N][N];
+
+            l = 0;
+            r = 0;
+            ans = 201;
+
+            for(int x = 0; x < N; x++) {
+                String[] input = br.readLine().split(" ");
+                for(int y = 0; y < N; y++) {
+                    int n = Integer.parseInt(input[y]);
+                    arr[x][y] = new Pos(x,y,n,n,n);
+                    r = max_r = Math.max(r,n);
+                }
+            }
+        }
+    }
+
+    /*
+        틀린 코드(메모리 초과) : visited를 min,max,x,y의 4차원으로 설정하여 방문 여부를 체크하는 것으로 메모리 초과가 발생한다.
+     */
+    public static class WrongSolve4 {
+        public class Pos {
+            int x,y,n,min_n,max_n;
+
+            Pos(int x, int y, int n, int min, int max) {
+                this.x = x;
+                this.y = y;
+                this.n = n;
+                this.min_n = min;
+                this.max_n = max;
+            }
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N,ans,l,r;
+        int[][] direction = {{0,-1},{-1,0},{0,1},{1,0}};
+        Pos[][] arr;
+
+        private void solve() throws IOException {
+            init_setting();
+
+            binary_search();
+
+            System.out.println(ans);
+        }
+
+        private void binary_search() {
+            if(l > r) return;
+
+            int m = (l + r) / 2;
+
+            if(bfs(arr[0][0], m)) {
+                r = m - 1;
+                ans = m;
+            }
+            else l = m + 1;
+
+            binary_search();
+        }
+
+        private boolean bfs(Pos s, int m) {
+            PriorityQueue<Pos> q = new PriorityQueue<>(new Comparator<Pos>() {
+                @Override
+                public int compare(Pos o1, Pos o2) {
+                    int o1_diff = o1.max_n - o1.min_n;
+                    int o2_diff = o2.max_n - o2.min_n;
+
+                    return o1_diff - o2_diff;
+                }
+            });
+            boolean[][][][] visited = new boolean[201][201][N][N];
+
+            q.add(s);
+            visited[s.n][s.n][s.x][s.y] = true;
+
+            while(!q.isEmpty()) {
+                Pos np = q.poll();
+
+                if(np.x == N - 1 && np.y == N - 1) {
+                    return true;
+                }
+
+                for(int[] d : direction) {
+                    int nx = np.x + d[0];
+                    int ny = np.y + d[1];
+
+                    if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+                    int nmin_n = Math.min(np.min_n, arr[nx][ny].n);
+                    int nmax_n = Math.max(np.max_n, arr[nx][ny].n);
+                    int diff = nmax_n - nmin_n;
+
+                    if(diff > m || visited[nmin_n][nmax_n][nx][ny]) continue;
+
+                    visited[nmin_n][nmax_n][nx][ny] = true;
+                    q.add(new Pos(nx,ny,arr[nx][ny].n,nmin_n,nmax_n));
+                }
+            }
+            return false;
+        }
+
+        private void init_setting() throws IOException {
+            N = Integer.parseInt(br.readLine());
+
+            arr = new Pos[N][N];
+
+            l = 0;
+            r = 0;
+            ans = 201;
+
+            for(int x = 0; x < N; x++) {
+                String[] input = br.readLine().split(" ");
+                for(int y = 0; y < N; y++) {
+                    int n = Integer.parseInt(input[y]);
+                    arr[x][y] = new Pos(x,y,n,n,n);
+                    r = Math.max(r,n);
+                }
+            }
+        }
+    }
+
+    /*
+        틀린 코드 : max-min의 값을 이진탐색하여 값을 정하고, BFS로 우선순위 큐를 사용하여 max-min의 값이 차이가 적은 순으로 이동 여부를 검사한다.
+        이때, max-min의 값과 x,y의 3차원의 방문 여부 배열을 사용하여 재방문을 조정한다.
+        하지만 해당 로직은 max-min의 값이 다르지만 결과값이 같은 경우는 구분하지 못하므로 최종적인 max-min의 값이 오류가 생길 수 있다.
+     */
+    public static class WrongSolve3 {
+        public class Pos {
+            int x,y,n,min_n,max_n;
+
+            Pos(int x, int y, int n, int min, int max) {
+                this.x = x;
+                this.y = y;
+                this.n = n;
+                this.min_n = min;
+                this.max_n = max;
+            }
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N,ans,l,r;
+        int[][] direction = {{0,-1},{-1,0},{0,1},{1,0}};
+        Pos[][] arr;
+
+        private void solve() throws IOException {
+            init_setting();
+
+            binary_search();
+
+            System.out.println(ans);
+        }
+
+        private void binary_search() {
+            if(l > r) return;
+
+            int m = (l + r) / 2;
+
+            if(bfs(arr[0][0], m)) {
+                r = m - 1;
+                ans = m;
+            }
+            else l = m + 1;
+
+            binary_search();
+        }
+
+        private boolean bfs(Pos s, int m) {
+            PriorityQueue<Pos> q = new PriorityQueue<>(new Comparator<Pos>() {
+                @Override
+                public int compare(Pos o1, Pos o2) {
+                    int o1_diff = o1.max_n - o1.min_n;
+                    int o2_diff = o2.max_n - o2.min_n;
+
+                    int diff = o1_diff - o2_diff;
+
+                    if(diff == 0) return o1.max_n - o2.max_n;
+                    else return diff;
+                }
+            });
+            boolean[][][] visited = new boolean[201][N][N];
+
+            q.add(s);
+            visited[0][s.x][s.y] = true;
+
+            while(!q.isEmpty()) {
+                Pos np = q.poll();
+
+                if(np.x == N - 1 && np.y == N - 1) {
+                    //ans = Math.min(ans, np.max_n - np.min_n);
+                    return true;
+                }
+
+                for(int[] d : direction) {
+                    int nx = np.x + d[0];
+                    int ny = np.y + d[1];
+
+                    if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+                    int nmin_n = Math.min(np.min_n, arr[nx][ny].n);
+                    int nmax_n = Math.max(np.max_n, arr[nx][ny].n);
+                    int diff = nmax_n - nmin_n;
+
+                    if(diff > m || visited[diff][nx][ny]) continue;
+
+                    visited[diff][nx][ny] = true;
+                    q.add(new Pos(nx,ny,arr[nx][ny].n,nmin_n,nmax_n));
+                }
+            }
+            return false;
+        }
+
+        private void init_setting() throws IOException {
+            N = Integer.parseInt(br.readLine());
+
+            arr = new Pos[N][N];
+
+            l = 0;
+            r = 0;
+            ans = 201;
+
+            for(int x = 0; x < N; x++) {
+                String[] input = br.readLine().split(" ");
+                for(int y = 0; y < N; y++) {
+                    int n = Integer.parseInt(input[y]);
+                    arr[x][y] = new Pos(x,y,n,n,n);
+                    r = Math.max(r,n);
+                }
+            }
+        }
+    }
+
+    /*
+        틀린 코드 : 이분 탐색의 대상을 max-min값으로 고정한 상태, visited를 int 타입으로 설정하여 해당 경로에 도달한 경우 max-min 값을 채워 기존의 값보다 작은 경로는 재방문이 가능하도록 하였지만
+        틀린 로직이다.
+     */
+    public static class WrongSolve2 {
         public class Pos {
             int x,y,n,min_n,max_n;
 
@@ -106,7 +442,6 @@ public class BOJ1981 {
                 Pos np = q.poll();
 
                 if(np.x == N - 1 && np.y == N - 1) {
-                    //q.clear();
                     return true;
                 }
 
@@ -155,6 +490,12 @@ public class BOJ1981 {
         }
     }
 
+    /*
+        틀린 코드 : 이분 탐색의 대상을 max-min값으로 고정한 상태, BFS에서 우선순위 큐를 통해 max-min값이 작은 값을 우선으로
+        (1,1) -> (N,N)으로의 이동 여부를 검사한다.
+        하지만, 해동 로직은 올바른 최소값을 보장하지 못한다.
+        한번의 이동이 최소값을 만족하지 못할 경우 다른 경로의 이동으로는 가능하지만 visited로 인해 재방문이 불가능하다.
+     */
     public static class WrongSolve {
         public class Pos {
             int x,y,n,min_n,max_n;
