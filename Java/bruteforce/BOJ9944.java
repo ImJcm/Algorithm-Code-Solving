d7ede8136d7c29575458e7778a842d5c09316a5a
ImJcm <wjsckdals0909@naver.com>
2026-01-14 12:23:36 +0900

    2026/01/14 12:23

--- Java/bruteforce/BOJ9944.java
+++ Java/bruteforce/BOJ9944.java
@@ -3,6 +3,7 @@ package bruteforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 /*
 NxM 보드 완주하기 다국어
@@ -65,6 +66,14 @@ PyPy3: 30 초
 Python 2: 30 초
 PyPy2: 30 초
  */
+/*
+알고리즘 핵심
+bruteforce
+1. 공의 이동횟수를 확인하기 위해 처음 시작위치를 보드 위의 가능한 지점 모두 수행해야 한다.
+2. 처음 시작 위치를 기준으로 4가지 방향으로 가능한 이동 방향을 수행하고, 불가능한 경우 백트랙킹을 수행한다.
+3. 모든 이동 가능한 지점을 도달하는 경우를 체크하기 위해 별도의 매개변수로 남은 블록을 체크하여 기저사례로 사용한다.
+4. 모든 블록을 도달한 경우, 최소 이동횟수를 업데이트한다.
+ */
 public class BOJ9944 {
     public static void main(String[] args) throws IOException {
         Solve task = new Solve();
@@ -73,16 +82,97 @@ public class BOJ9944 {
 
     public static class Solve {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N,M;
-        int[][] board;
+        int N,M,ans,remain_block,num = 1;
+        char[][] board;
+        boolean[][] visited;
+        int[][] direction = {
+                {1,0},{-1,0},{0,1},{0,-1}
+        };
 
         private void solve() throws IOException {
-            init_setting();
+            while(true) {
+                if(init_setting()) return;
 
+                for(int i = 0; i < N; i++) {
+                    for(int j = 0; j < M; j++) {
+                        if(Objects.equals(board[i][j],'*')) continue;
+                        dfs(i,j,0,remain_block - 1);
+                    }
+                }
+
+                System.out.printf("Case %d: %d\n",num++,ans == Integer.MAX_VALUE ? -1 : ans);
+            }
         }
 
-        private void init_setting() throws IOException {
+        private void dfs(int i, int j, int mv, int rb) {
+            if(rb == 0) {
+                ans = Math.min(ans, mv);
+                return;
+            }
 
+            if(mv > ans || mv > 1_000_000) return;
+
+            for(int[] d : direction) {
+                int ni;
+                int nj;
+                int w = 1;
+
+                while(true) {
+                    ni = i + d[0] * w;
+                    nj = j + d[1] * w;
+
+                    if(ni < 0 || ni >= N || nj < 0 || nj >= M || visited[ni][nj]) {
+                        w--;
+                        break;
+                    } else {
+                        w++;
+                    }
+                }
+
+                if(w == 0) continue;
+
+                ni = i + d[0] * w;
+                nj = j + d[1] * w;
+
+                for(int k = 0; k <= w; k++) {
+                    visited[i + d[0] * k][j + d[1] * k] = true;
+                }
+
+                dfs(ni,nj,mv + 1, rb - w);
+
+                for(int k = 0; k <= w; k++) {
+                    visited[i + d[0] * k][j + d[1] * k] = false;
+                }
+            }
+        }
+
+        private boolean init_setting() throws IOException {
+            String line = br.readLine();
+
+            if(line == null) return true;
+
+            String[] input = line.split(" ");
+
+            N = Integer.parseInt(input[0]);
+            M = Integer.parseInt(input[1]);
+
+            board = new char[N][M];
+            visited = new boolean[N][M];
+
+            remain_block = 0;
+
+            for(int i = 0; i < N; i++) {
+                input = br.readLine().split("");
+                for(int j = 0; j < M; j++) {
+                    char ch = input[j].charAt(0);
+                    if(ch == '.') remain_block++;
+                    board[i][j] = ch;
+                    visited[i][j] = (ch == '*');
+                }
+            }
+            ans = Integer.MAX_VALUE;
+
+            return false;
         }
     }
 }
