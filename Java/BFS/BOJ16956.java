d56c842f524f66cee03d4db70a0beafffdc63e4d
ImJcm <wjsckdals0909@naver.com>
2026-01-27 20:45:59 +0900

    2026/01/27 20:45

--- Java/BFS/BOJ16956.java
+++ Java/BFS/BOJ16956.java
@@ -3,6 +3,9 @@ package BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /*
 늑대와 양 스페셜 저지
@@ -64,6 +67,15 @@ S.D..
 출처
 문제를 번역한 사람: baekjoon
  */
+/*
+알고리즘 핵심
+BFS
+1. 입력으로 주어지는 목장의 상태에서 빈공간을 울타리로 모두 채운다. (BFS 수행)
+2. 이때, 양과 늑대의 접촉여부를 확인하기 위해 목장의 모든 각각의 양을 기준으로 상하좌우에 늑대가 존재하는지 검사한다.
+3. 2번에서 늑대가 존재하면 울타리를 설치해도 양과 늑대의 접촉이 불가피하므로 0을 출력하고, 그렇지 않으면 1을 출력하고 목장의 상태를 출력한다.
+
+힌트에서 주어진 문장처럼 울타리를 어떻게 설치하는지는 상관이 없으므로 모든 공간에 울타리를 설치하고 상태를 확인하는 것이 이 문제의 핵심이다.
+ */
 public class BOJ16956 {
     public static void main(String[] args) throws IOException {
         Solve task = new Solve();
@@ -71,14 +83,106 @@ public class BOJ16956 {
     }
 
     public static class Solve {
+        static class Pos {
+            int r,c;
+
+            Pos(int r, int c) {
+                this.r = r;
+                this.c = c;
+            }
+        }
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int R,C,sheep_cnt, wolf_cnt;
+        char[][] ranch;
+        int[][] direction = new int[][] {{1,0},{-1,0},{0,1},{0,-1}};
+        ArrayList<Pos> es;   // empty space = '.'
+
         private void solve() throws IOException {
             init_setting();
 
+            bfs();
+
+            check();
+        }
+
+        private void check() {
+            for(int i = 0; i < R; i++) {
+                for(int j = 0; j < C; j++) {
+                    if(ranch[i][j] == 'S' && !near_check(i,j)) {
+                        System.out.println(0);
+                        return;
+                    }
+                }
+            }
+
+            System.out.println(1);
+            print_ranch();
+        }
+
+        private void print_ranch() {
+            for(int i = 0; i < R; i++) {
+                System.out.println(ranch[i]);
+            }
+        }
+
+        private boolean near_check(int r, int c) {
+            for(int[] d : direction) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if(nr < 0 || nr >= R || nc < 0 || nc >= C || ranch[nr][nc] == 'D') continue;
+                if(ranch[nr][nc] == 'W') return false;
+            }
+
+            return true;
+        }
+
+        private void bfs() {
+            Queue<Pos> q = new LinkedList<>(es);
+            boolean[][] visited = new boolean[R][C];
+
+            while(!q.isEmpty()) {
+                Pos now = q.poll();
+
+                if(visited[now.r][now.c]) continue;
+                visited[now.r][now.c] = true;
+                ranch[now.r][now.c] = 'D';
+
+                for(int[] d : direction) {
+                    int nr = now.r + d[0];
+                    int nc = now.c + d[1];
+
+                    if(nr < 0 || nr >= R || nc < 0 || nc >= C || visited[nr][nc]) continue;
+                    if(ranch[nr][nc] == 'S' || ranch[nr][nc] == 'W') continue;
+
+                    q.add(new Pos(nr,nc));
+                }
+            }
         }
 
         private void init_setting() throws IOException {
+            String[] input = br.readLine().split(" ");
 
+            R = Integer.parseInt(input[0]);
+            C = Integer.parseInt(input[1]);
+
+            sheep_cnt = 0;
+            wolf_cnt = 0;
+
+            ranch = new char[R][C];
+            es = new ArrayList<>();
+
+            for(int i = 0; i < R; i++) {
+                input = br.readLine().split("");
+                for(int j = 0; j < C; j++) {
+                    char ch = input[j].charAt(0);
+                    ranch[i][j] = ch;
+
+                    if(ch == 'S') sheep_cnt++;
+                    if(ch == 'W') wolf_cnt++;
+                    if(ch == '.') es.add(new Pos(i,j));
+                }
+            }
         }
     }
 }
