97e73d87f8834ec0d1026eb882b6158141501e80
ImJcm <wjsckdals0909@naver.com>
2026-02-11 00:25:23 +0900

    2026/01/23 01:13, change the access specifier

--- Java/bruteforce/BOJ7453.java
+++ Java/bruteforce/BOJ7453.java
@@ -59,13 +59,13 @@ public class BOJ7453 {
         task.solve();
     }
 
-    public static class Solve {
+    private static class Solve {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N;
         long ans;
         int[] A,B,C,D,AB,CD;
 
-        private void solve() throws IOException {
+        void solve() throws IOException {
             init_setting();
 
             //two_point_check();
