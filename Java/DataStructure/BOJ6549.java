ee93da9c1b36bcb67873e299a8b7d5c24a14fef5
ImJcm <wjsckdals0909@naver.com>
2026-02-24 10:50:30 +0900

    prototype : ref help code

--- Java/DataStructure/BOJ6549.java
+++ Java/DataStructure/BOJ6549.java
@@ -3,6 +3,7 @@ package DataStructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /*
 히스토그램에서 가장 큰 직사각형 다국어
@@ -49,14 +50,109 @@ public class BOJ6549 {
 
     private static class Solve {
         private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private int N;
+        private int[] heights;
+        private StringBuilder ans = new StringBuilder();
 
         void solve() throws IOException {
-            init_setting();
 
         }
 
-        private void init_setting() throws IOException {
+        private boolean init_setting() throws IOException {
+            String[] input = br.readLine().split(" ");
 
+            if(input[0].equals("0")) return false;
+
+            N = Integer.parseInt(input[0]);
+
+            heights = Arrays.stream(input,1,input.length)
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            return true;
+        }
+    }
+
+    /*
+        질문게시판 코드 참조
+     */
+    private static class Solve_helper {
+        private class Segment_tree {
+            private int[] s_tree;
+
+            public Segment_tree(int[] arr) {
+                s_tree = new int[4 * arr.length];
+                init(arr,0,arr.length - 1,1);
+            }
+
+            // [s,e] 구간의 최소 높이의 인덱스를 저장하는 세그먼트 트리 생성
+            private int init(int[] arr, int s, int e, int n) {
+                if(s == e) return s_tree[n] = s;
+                int m = (s + e) / 2;
+                int l = init(arr, s, m, 2 * n);
+                int r = init(arr, m + 1, e, 2 * n + 1);
+                return s_tree[n] = arr[l] < arr[r] ? l : r;
+            }
+
+            // [s,e] 구간 내에서 또다른 부분 구간 [l,r]에서의 최소 높이를 갖는 인덱스를 반환
+            int query(int[] arr, int l, int r, int s, int e, int n) {
+                if(l > e || r < s) return -1;
+                if(l >= s && r <= e) return s_tree[n];
+                int m = (l + r) / 2;
+                int nl = query(arr, l, m, s, e, n * 2);
+                int nr = query(arr, m + 1, r, s, e, n * 2 + 1);
+                if(nl == -1) return nr;
+                if(nr == - 1) return nl;
+                return arr[nl] < arr[nr] ? nl : nr;
+            }
+        }
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private int N;
+        private int[] heights;
+        private Segment_tree stree;
+        private StringBuilder ans = new StringBuilder();
+
+        void solve() throws IOException {
+            while(init_setting()) {
+                ans.append(find_largest_rectangle(heights, 0, heights.length - 1)).append("\n");
+            }
+
+            System.out.println(ans.toString());
+        }
+
+        /*
+            특정 구간에서 최소 높이를 찾은 후, 최소 높이 인덱스를 기준으로 구간을 나누어 최대 넓이를 구한 후, 이를 반복하여 최대 넓이 값을 구한다.
+         */
+        private int find_largest_rectangle(int[] h, int s, int e) {
+            int lower_height_idx = stree.query(h,0, h.length - 1, s, e, 1);
+
+            int area = (e - s + 1) * h[lower_height_idx];
+
+            if(s <= lower_height_idx - 1) {
+                area = Math.max(area, find_largest_rectangle(h, s, lower_height_idx - 1));
+            }
+
+            if(lower_height_idx + 1 <= e) {
+                area = Math.max(area, find_largest_rectangle(h, lower_height_idx + 1, e));
+            }
+
+            return area;
+        }
+
+        private boolean init_setting() throws IOException {
+            String[] input = br.readLine().split(" ");
+
+            if(input[0].equals("0")) return false;
+
+            N = Integer.parseInt(input[0]);
+
+            heights = Arrays.stream(input,1,input.length)
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            stree = new Segment_tree(heights);
+
+            return true;
         }
     }
 }
