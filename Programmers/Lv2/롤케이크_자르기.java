cda1ea3596c645b5f19a9325047007030a566734
ImJcm <wjsckdals0909@naver.com>
2026-04-20 22:13:16 +0900

    2026/04/20 22:13

--- Programmers/Lv2/롤케이크_자르기.java
+++ Programmers/Lv2/롤케이크_자르기.java
@@ -1,5 +1,10 @@
 package Programmers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 /*
 문제 설명
 철수는 롤케이크를 두 조각으로 잘라서 동생과 한 조각씩 나눠 먹으려고 합니다. 이 롤케이크에는 여러가지 토핑들이 일렬로 올려져 있습니다. 철수와 동생은 롤케이크를 공평하게 나눠먹으려 하는데, 그들은 롤케이크의 크기보다 롤케이크 위에 올려진 토핑들의 종류에 더 관심이 많습니다. 그래서 잘린 조각들의 크기와 올려진 토핑의 개수에 상관없이 각 조각에 동일한 가짓수의 토핑이 올라가면 공평하게 롤케이크가 나누어진 것으로 생각합니다.
@@ -23,6 +28,71 @@ topping	result
 
 롤케이크를 공평하게 나눌 수 없습니다.
  */
-class 롤케이크_자르기 {
+public class 롤케이크_자르기 {
+    public static void main() throws IOException{
+        Solution sol = new Solution();
+        sol.solve();
+    }
 
+    private static class Solution {
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private int[] topping,left_kind,right_kind;
+        private boolean[] left_visited, right_visited;
+        private int ans;
+
+        void solve() throws IOException {
+            init_setting();
+
+            int ans = solution(topping);
+
+            System.out.println(ans);
+        }
+
+        int solution(int[] topping) {
+            check_kind(topping);
+
+            for(int i = 0; i < topping.length - 1; i++) {
+                if(left_kind[i] == right_kind[i + 1]) ans++;
+            }
+
+            return ans;
+        }
+
+        private void check_kind(int[] topping) {
+            left_visited[topping[0]] = true;
+            left_kind[0] = 1;
+            right_visited[topping[topping.length - 1]] = true;
+            right_kind[topping.length - 1] = 1;
+
+            for(int i = 1; i < topping.length; i++) {
+                if(left_visited[topping[i]]) left_kind[i] = left_kind[i - 1];
+                else {
+                    left_kind[i] = left_kind[i - 1] + 1;
+                    left_visited[topping[i]] = true;
+                }
+
+                if(right_visited[topping[topping.length - 1 - i]]) right_kind[topping.length - 1 - i] = right_kind[topping.length - i];
+                else {
+                    right_kind[topping.length - 1 - i] = right_kind[topping.length - i] + 1;
+                    right_visited[topping[topping.length - 1 - i]] = true;
+                }
+            }
+        }
+
+        private void init_setting() throws IOException {
+            String input = br.readLine().replaceAll("[\\[\\]]","");
+
+            topping = Arrays.stream(input.split(", "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            left_kind = new int[topping.length];
+            right_kind = new int[topping.length];
+
+            left_visited = new boolean[10001];
+            right_visited = new boolean[10001];
+
+            ans = 0;
+        }
+    }
 }
