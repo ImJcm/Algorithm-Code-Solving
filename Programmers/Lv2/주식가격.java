1b365603dc26bbe843d5bd80190f91e24b9371ab
ImJcm <wjsckdals0909@naver.com>
2026-05-21 05:24:30 +0900

    2026/05/21 05:24

--- Programmers/Lv2/주식가격.java
+++ Programmers/Lv2/주식가격.java
@@ -25,19 +25,35 @@ prices	return
  */
 /*
 알고리즘 핵심
+Stack(DataStructure)
+1. 초단위의 순차적으로 주식 가격이 주어지므로 해당 주식이 떨어지는 시간을 구하기 위해 현재의 가격과 이전 가격의 비교가 필요하다.
+2. 스택을 통해 시간대별 주식의 가격을 저장하다가 현재 가격이 이전 가격과 비교한다.
+2-1. 현재 가격이 이전 가격보다 적다면, 이전 시간대의 주식의 가격이 떨어지는 최초의 시기이므로 현재 시간에서 이전 가격대의 시간의 차이를 저장한다.
+2-2. 현재 가격이 이전 가격보다 크다면, 이전 시간대의 주식의 가격은 떨어지지 않았으므로 현재 시간대의 주식의 가격을 스택에 저장한다.
+3. 2번 과정을 모두 마친후, 떨어지는 시간대를 측정한 후, 남은 시간대의 주식가격은 최종시간과의 차이를 저장하여 모든 시간대를 구한다.
  */
 public class 주식가격 {
     public static void main(String[] args) {
         int[] prices = {
                 1,2,3,2,3
         };
+
         Solve task = new Solve();
         System.out.println(Arrays.toString(task.solution(prices)));
     }
 
     private static class Solve {
+        private class StockData {
+            int price;
+            int time;
+
+            public StockData(int price, int time) {
+                this.price = price;
+                this.time = time;
+            }
+        }
         private int[] ans;
-        private Stack<Integer> stack;
+        private Stack<StockData> stack;
 
         public int[] solution(int[] prices) {
             init_setting(prices);
@@ -48,11 +64,33 @@ public class 주식가격 {
         }
 
         private void stock(int[] prices) {
+            stack.push(new StockData(prices[0], 1));
 
+            for(int i = 1; i < prices.length; i++) {
+
+                while(!stack.isEmpty() && stack.peek().price > prices[i]) {
+                    StockData sd = stack.pop();
+
+                    ans[sd.time - 1] = i + 1 - sd.time;
+                }
+                stack.push(new StockData(prices[i], i + 1));
+            }
+
+            for(int i = 0; i < prices.length; i++) {
+                int et = prices.length;
+
+                while(!stack.isEmpty()) {
+                    StockData sd = stack.pop();
+
+                    ans[sd.time - 1] = et - sd.time;
+                }
+            }
         }
 
         private void init_setting(int[] prices) {
             ans = new int[prices.length];
+
+            Arrays.fill(ans, -1);
 
             stack = new Stack<>();
         }
