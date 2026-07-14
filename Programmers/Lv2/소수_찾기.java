eb5a4457d817c2bf2f70c2eab627f53bb9b145f6
ImJcm <wjsckdals0909@naver.com>
2026-07-14 23:18:54 +0900

    2026/07/14 23:18

--- Programmers/Lv2/소수_찾기.java
+++ Programmers/Lv2/소수_찾기.java
@@ -25,29 +25,73 @@ numbers	return
 
 11과 011은 같은 숫자로 취급합니다.
  */
+/*
+알고리즘 핵심
+에라토스테네스의 체 + 완전 탐색
+1. 최대 7자리 수로 만들 수 있는 소수를 찾기 때문에 10_000_000이하의 값까지 소수를 에라스토테네스의 체로 구한다.
+2. numbers로 구할 수 있는 중복없는 모든 경우의 숫자인 문자열을 소수인지 판별한 후, ans를 업데이트한다.
+(numbers에서 해당 값을 넣는 경우와 안넣는 경우 모두 고려하고 중복이 없어야 한다.)
+ */
 public class 소수_찾기 {
     static void main() {
-        String numbers = "011";
+        String numbers = "143";
 
         Solve task = new Solve();
         System.out.println(task.solution(numbers));
     }
 
     private static class Solve {
+        private final int NUMBER_LIMIT = 10_000_000;
         private int ans;
+        private boolean[] decimal,visited,prime_visited;
 
         public int solution(String numbers) {
             init_setting(numbers);
 
-            // 에레스토테네스의 체 = 소수 배열 구하기
+            seive_of_eratosthenes();
 
-            // DFS or BFS로 numbers로 만들 수 있는 숫자인 문자열 구하고 소수 판별
+            make_decimal(0, "", numbers.toCharArray());
 
             return ans;
         }
+        
+        private void make_decimal(int l, String num, char[] target) {
+            if(l == target.length) {
+                if(num.isEmpty()) return;
+                int res = Integer.parseInt(num);
+                if(!decimal[res] && !prime_visited[res]) {
+                    prime_visited[res] = true;
+                    ans++;
+                }
+                return;
+            }
+
+            for(int i = 0; i < target.length; i++) {
+                make_decimal(l + 1, num, target);
+                if(visited[i]) continue;
+                visited[i] = true;
+                String nnum = num + target[i];
+                make_decimal(l + 1, nnum, target);
+                visited[i] = false;
+            }
+        } 
+
+        private void seive_of_eratosthenes() {
+            for(int i = 2; i < NUMBER_LIMIT / 2; i++) {
+                for(int j = 2; i * j < NUMBER_LIMIT; j++) {
+                    decimal[i * j] = true;
+                }
+            }
+        }
 
         private void init_setting(String numbers) {
+            decimal = new boolean[NUMBER_LIMIT];
+            visited = new boolean[numbers.length()];
+            prime_visited = new boolean[NUMBER_LIMIT];
 
+            decimal[0] = decimal[1] = true;
+            prime_visited[0] = prime_visited[1] = true;
+            ans = 0;
         }
     }
 }
