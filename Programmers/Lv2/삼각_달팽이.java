4ee1ee233e4f63d9965c1359016c6e1d39253d49
ImJcm <wjsckdals0909@naver.com>
2026-08-03 22:06:39 +0900

    2026/08/03 22:06

--- Programmers/Lv2/삼각_달팽이.java
+++ Programmers/Lv2/삼각_달팽이.java
@@ -1,5 +1,7 @@
 package Lv2;
 
+import java.util.Arrays;
+
 /*
 삼각 달팽이
 제출 내역
@@ -26,8 +28,96 @@ n	result
 
 문제 예시와 같습니다.
  */
+/*
+
+ */
 public class 삼각_달팽이 {
     static void main() {
+        int[] n = new int[] {
+                1,2,4,5,6,7,8,9
+        };
 
+        Solve task = new Solve();
+        System.out.println(Arrays.toString(task.solution(n[7])));
     }
+
+    private static class Solve {
+        private int L;
+        private int[] ans;
+        private int[] snail;
+
+        public int[] solution(int n) {
+            init_setting(n);
+
+            make_triangle_snail(n);
+
+            return ans;
+        }
+
+        private void make_triangle_snail(int n) {
+            int num = 1, w = 0, c = 1;
+            int l_idx, m_idx, r_idx = 0;
+
+            while (true) {
+                if(num > L) break;
+                // left side
+                l_idx = c == 1 ? 0 : (r_idx + w + 1);
+                w = c;
+                l_idx += w;
+                while (snail[l_idx] == 0) {
+                    snail[l_idx] = num++;
+                    l_idx += (w++ + c - 1);
+                }
+
+                // bottom
+                m_idx = l_idx - (w + c - 1) + 2;
+                while (snail[m_idx] == 0) {
+                    snail[m_idx++] = num++;
+                }
+
+                // right side
+                r_idx = (m_idx - 1) - (n - c + 1);
+                w = n - 1;
+                while (snail[r_idx] == 0) {
+                    snail[r_idx] = num++;
+                    r_idx -= (w-- - c + 1);
+                }
+                c++;
+            }
+
+            ans = Arrays.stream(snail, 1, snail.length - 1)
+                    .toArray();
+        }
+
+        private void init_setting(int n) {
+            L = permutaion_sum(n);
+            ans = new int[L];
+            snail = new int[L + 2];
+
+            snail[0] = snail[L + 1] = -1;
+        }
+
+        private int permutaion_sum(int n) {
+            return n * (n + 1) / 2;
+        }
+
+        private void testPrint(int n) {
+            int l = 1;
+            for(int i = 1; i <= n; i++) {
+                System.out.printf(" ".repeat((n - i)));
+                for(int j = 0; j < i; j++) {
+                    if(snail[l] > 9) System.out.printf("%d ",snail[l++]);
+                    else System.out.printf("%d  ", snail[l++]);
+                }
+                System.out.println();
+            }
+        }
+    }
+    // 1 2  3 4  5  6 7  8  9 10 11 12 13 14 15
+    // 1 2 12 3 13 11 4 14 15 10  5  6  7  8  9
+    //      o    o  o    o  o  o
+
+    // 1 2  3 4  5  6 7  8  9 10 11 12 13 14 15 16 17 18 19 20 21
+    // 1 2 15 3 16 14 4 17 21 13  5 18 19 20 12  6  7  8  9 10 11
+    //      o    o  o    o  o  o     o  o  o
 }
