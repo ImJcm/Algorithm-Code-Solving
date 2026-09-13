53de871185c32b7d729f3fd1a5455eaa6abdbfac
ImJcm <wjsckdals0909@naver.com>
2026-09-14 01:26:16 +0900

    2026/09/14 01:26

--- Programmers/Lv3/여행경로.java
+++ Programmers/Lv3/여행경로.java
@@ -34,14 +34,26 @@ tickets	return
 힌트가 필요한가요? [코딩테스트 연습 힌트 모음집]으로 오세요!
 - https://school.programmers.co.kr/learn/courses/14743/14743-%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%97%B0%EC%8A%B5-%ED%9E%8C%ED%8A%B8-%EB%AA%A8%EC%9D%8C%EC%A7%91?itm_content=lesson43164
  */
+/*
+알고리즘 핵심
+DFS
+1. tickets을 목적지를 기준으로 오름차순을 적용한 순서로 공항마다 티켓들을 저장하고, 방문여부를 저장하기 위해 별도의 각각의 배열을 갖는다.
+2. 출발지인 "ICN"을 시작으로 티켓을 순차적으로 조회하여 다음 목적지 공항을 DFS를 통해 경로를 구성한다.
+3. 이때, 모든 티켓을 사용하므로 기저사례는 생성된 DFS 호출문의 개수가 모든 티켓의 개수와 동일한 조건을 설정한다.
+4. 처음으로 조건을 만족하는 경로가 생성되면 모든 DFS 호출을 종료해야 하므로, 별도의 flag 변수를 설정하여 기저사례를 추가한다.
+
+처음 접근으로 단순하게 알파벳 순으로 빠른 목적지를 설정하기만 하면 경로가 설정될 줄 알았지만 그렇지 않았다.
+문제가 요구하는 것은 모든 티켓을 사용한 경로 중 알파벳 순으로 빠른 경로를 찾는 것이므로 오름차순 정렬 티켓 + 방문 여부를
+검사하여 경로를 찾아야 되는 것이었다.
+ */
 public class 여행경로 {
     static void main() {
         String[][] tickets = new String[][] {
                 //{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}
                 //{"ICN", "JFK"}, {"ICN", "JFK"}, {"JFK", "HND"}, {"HND", "ICN"}, {"JFK", "ATL"}
-                //{"ICN", "BBB"}, {"BBB", "ICN"}, {"ICN", "AAA"}
+                {"ICN", "BBB"}, {"BBB", "ICN"}, {"ICN", "AAA"}
                 //{"ICN", "AAA"}, {"AAA", "ICN"}, {"ICN", "CCC"}, {"CCC", "ICN"}, {"ICN", "DDD"}, {"DDD", "AAA"}
-                {"EZE","TIA"},{"EZE","HBA"},{"AXA","TIA"},{"ICN","AXA"},{"ANU","ICN"},{"ADL","ANU"},{"TIA","AUA"},{"ANU","AUA"},{"ADL","EZE"},{"ADL","EZE"},{"EZE","ADL"},{"AXA","EZE"},{"AUA","AXA"},{"ICN","AXA"},{"AXA","AUA"},{"AUA","ADL"},{"ANU","EZE"},{"TIA","ADL"},{"EZE","ANU"},{"AUA","ANU"}
+                //{"EZE","TIA"},{"EZE","HBA"},{"AXA","TIA"},{"ICN","AXA"},{"ANU","ICN"},{"ADL","ANU"},{"TIA","AUA"},{"ANU","AUA"},{"ADL","EZE"},{"ADL","EZE"},{"EZE","ADL"},{"AXA","EZE"},{"AUA","AXA"},{"ICN","AXA"},{"AXA","AUA"},{"AUA","ADL"},{"ANU","EZE"},{"TIA","ADL"},{"EZE","ANU"},{"AUA","ANU"}
         };
 
         Solve task = new Solve();
@@ -101,7 +113,7 @@ public class 여행경로 {
                 if(airports.get(pos).visited.get(i)) continue;
 
                 airports.get(pos).visitTickets(i);
-                travel_route(++idx, route_cnt, airports.get(pos).tickets.get(i), airports);
+                travel_route(idx + 1, route_cnt, airports.get(pos).tickets.get(i), airports);
                 airports.get(pos).unVisitTickets(i);
             }
         }
